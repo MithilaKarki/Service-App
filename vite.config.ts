0343273d3009fb44c3227acad89f8bf7bd46d4cd
Git002KaRki <karkimithila480@gmail.com>
2026-09-15 16:55:57 +0545

    docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//      tailwindcss(),
+//   ],
+//   server: {
+//       port: 7000
+//     }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,6 +22,7 @@ export default defineConfig({
      tailwindcss(),
   ],
   server: {
+      host: '0.0.0.0',
       port: 7000
     }
 })
